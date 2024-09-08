9c32bbe51a6d2623dd7d162a1a3eb6bba761a849
Alenochka2003 <alena.filina@icloud.com>
2024-09-08 19:35:05 +0200

    comm

---
+++ models/App.js
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
+
+const App = sequelize.define('App', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  tableName: 'Apps',
+  timestamps: false // отключаем автоматическое добавление createdAt и updatedAt
+});
+
+export default App;
